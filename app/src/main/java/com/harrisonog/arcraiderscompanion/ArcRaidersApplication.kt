672f91146fdff7b646e7b4f2f65c1049c9b8eeca
Harrison Oglesby <oglesby.harrison@gmail.com>
2025-12-31 19:56:09 -0800

    Fix item loading issue

--- app/src/main/java/com/harrisonog/arcraiderscompanion/ArcRaidersApplication.kt
+++ app/src/main/java/com/harrisonog/arcraiderscompanion/ArcRaidersApplication.kt
@@ -1,7 +1,18 @@
 package com.harrisonog.arcraiderscompanion
 
 import android.app.Application
+import com.harrisonog.arcraiderscompanion.data.initialization.AppInitializer
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class ArcRaidersApplication : Application()
+class ArcRaidersApplication : Application() {
+
+    @Inject
+    lateinit var appInitializer: AppInitializer
+
+    override fun onCreate() {
+        super.onCreate()
+        appInitializer.initialize()
+    }
+}
